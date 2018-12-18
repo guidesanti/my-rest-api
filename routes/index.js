7cc02a52a244d8b9ad5e6f3a9e357775c59db5b8
Guilherme A. de Santi <guidesanti@yahoo.com.br>
2018-12-17 23:40:49 -0200

    [#6] General enhancements
    Changes:
    - Updated all the general endpoints to use the new response format

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,30 @@
 var express = require('express');
 var router = express.Router();
 var HttpStatus = require('http-status-codes');
-var package = require('../package.json');
-var config = require('../config/config.js').getAppConfig();
+var appRoot = require('app-root-path');
+
+var package = require(appRoot + '/package.json');
+var config = require(appRoot + '/config/config.js').getAppConfig();
+var Response = require(appRoot + '/transport/response');
 
 router.get('/', function(req, res, next) {
-  res.json({ data: 'This is my rest api' });
+  res.status(HttpStatus.OK);
+  res.json(Response.success({ message: 'This is my rest api' }));
 });
 
 router.get('/health', function (req, res, next) {
   res.status(HttpStatus.OK);
-  res.json({status: "Running", version: package.version});
+  res.json(Response.success({ status: 'Running', version: package.version }));
 });
 
 router.get('/health/version', function(req, res, next) {
   res.status(HttpStatus.OK);
-  res.json({ version: package.version });
-  res.end();
+  res.json(Response.success({ version: package.version }));
 });
 
 router.get('/config', function(req, res, next) {
   res.status(HttpStatus.OK);
-  res.json(config);
-  res.end();
+  res.json(Response.success(config));
 });
 
 module.exports = router;
